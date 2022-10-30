5721dbafc8c1bc1c39bd7df4d0b9ea95f6c9de1b
proninmo <proninmo@gmail.com>
2022-10-30 14:22:09 +0300

    token fix

--- config.tf
+++ config.tf
@@ -9,7 +9,7 @@ terraform {
 
 // Configure the Yandex.Cloud provider
 provider "yandex" {
-  token                    = "y0_AgAAAAAFB4j6AATuwQAAAADSlhucqqU6X_zsRh61zgMVgtmNPBj-G0U"
+  token                    = "t1.9euelZrJjM6SyZ3Nm8vKzcaPl8qej-3rnpWaj5WazI-SkJmbm8meyc6Ni8fl8_c6QwZl-e8eaF19_d3z93pxA2X57x5oXX39zef1656Vmo_OlMrOkM2Tmo2JyJHKzpma7_0.2CxWo92opY0n8WoIEwfEa2gw2UT-9_SBzW2X-exfTPQa94h6F_J6B5IEy8EwLf7zOC60sWmzWzGbS8B9F_IsDA"
   service_account_key_file = "/home/fddi/key.json"
   cloud_id                 = "b1g5h324cthaptno5nqf"
   folder_id                = "b1g2f8n81gpa25g6i9sg"
