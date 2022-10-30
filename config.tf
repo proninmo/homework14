740e8724fe14afab0ec9c64826825ef2cf70d98a
proninmo <proninmo@gmail.com>
2022-10-30 15:05:58 +0300

    add subnet

--- config.tf
+++ config.tf
@@ -22,6 +22,14 @@ data "yandex_compute_image" "container-optimized-image" {
   family = "container-optimized-image"
 }
 
+resource "yandex_vpc_subnet" "foo" {
+  name           = "subnet-1"
+  description    = "My first subnet"
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.foo.id}"
+}
+
 resource "yandex_compute_instance" "instance-based-on-coi" {
   boot_disk {
     initialize_params {
