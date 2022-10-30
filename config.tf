4cbbe702db58cba898f258ff4a56bca71fad0572
proninmo <proninmo@gmail.com>
2022-10-30 15:48:00 +0300

    fix net

--- config.tf
+++ config.tf
@@ -74,7 +74,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  v4_cidr_blocks = ["10.2.3.0/24"]
 }
 
 output "internal_ip_address_vm_1" {
