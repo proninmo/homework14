15673ef7dd4bf73ca71a8d95f61f1668325ed9af
proninmo <proninmo@gmail.com>
2022-10-31 10:29:21 +0300

    fix image params

--- config.tf
+++ config.tf
@@ -19,50 +19,29 @@ provider "yandex" {
 // Create a new instance
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+
+  name        = "linux-vm"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
 
   resources {
-    cores  = 2
-    memory = 2
+    cores  = "2"
+    memory = "2"
   }
 
   boot_disk {
     initialize_params {
-      image_id = "f2eqqnlf8ugqu0hol5s5"
+      image_id = "fd8egv6phshj1f64q94n"
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = "${yandex_vpc_subnet.subnet-1.id}"
     nat       = true
   }
 
   metadata = {
-    ssh-keys = "fddi:${file("/home/fddi/.ssh/id_rsa.pub")}"
-  }
-}
-
-resource "yandex_compute_instance" "vm-2" {
-  name = "terraform2"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "f2eqqnlf8ugqu0hol5s5"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "fddi:${file("/home/fddi/.ssh/id_rsa.pub")}"
+    ssh-keys = "fddi:AAAAB3NzaC1yc2EAAAADAQABAAABgQDyVn+C/voNdyZmT+ze221T7DQtAVQ8Ymz3bdiTvyIcsmNzd4yVNZFiAdMbbpEHYg8YnkT7ZASXo7ySk4NFSZQP3yIYmPui79tB5Ok0jYXG8zlA03B0QyIh7VfeKrHxt9QI77detGaRWpdkzGQPf496mmvzHud4XhT4bswurBfzOK5c70eZ9puCz9FNiv33VdKJe8lTu/468LE5R+e4ipz8C8X7f9+sxXLgS/42eZd6scWEU6EKF+WS5x7+yjLKhXUZvrs10fVOIlIym2Lqta4stEugcmX5zC5t1RjHfrHcCxrtmSThGYMYrpCl7o9aESCMLvMnQoVSJoL0MobgEW+Mkg1srGWNZca7B57FfOkRU4VsR9FrpGS49n+vsNA7ObZ9DgvNe0Nok5zK74MPeOybz9ZmZB3eXRXMrJK+eB3MNvkQ2hLDdz96L993IkHaNmvWvT0H8RwUvMitsnMPiINa179Q4h3MX0ZkI7lsJLrtB4fZePP0VH01f8J3mfphQf0="
   }
 }
 
@@ -72,24 +51,7 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["10.2.3.0/24"]
-}
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
-}
-
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
-
-output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+  zone           = "<зона доступности>"
+  v4_cidr_blocks = ["10.2.0.0/24"]
+  network_id     = "${yandex_vpc_network.network-1.id}"
 }
