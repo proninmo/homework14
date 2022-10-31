2c35d7f732bfc3499673551af75497eef927ad9e
proninmo <proninmo@gmail.com>
2022-10-31 10:31:45 +0300

    fix zone

--- config.tf
+++ config.tf
@@ -51,7 +51,7 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-  zone           = "<зона доступности>"
+  zone           = "ru-central1-a"
   v4_cidr_blocks = ["10.2.0.0/24"]
   network_id     = "${yandex_vpc_network.network-1.id}"
 }
